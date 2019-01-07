8c8d13c070ee68c18de78e42d2b4396337167da4
editzonl <editzon_lg@hotmail.com>
2019-01-06 22:30:07 -0500

    creacion de los paneles

--- n5_planeacionUrbana/source/uniandes/cupi2/planeacionUrbana/interfaz/InterfazPlaneacionUrbana.java
+++ n5_planeacionUrbana/source/uniandes/cupi2/planeacionUrbana/interfaz/InterfazPlaneacionUrbana.java
@@ -1,5 +1,29 @@
 package uniandes.cupi2.planeacionUrbana.interfaz;
 
-public class InterfazPlaneacionUrbana {
+import javax.swing.JFrame;
+
+/**
+ * Interfaz del juego planeacion urbana.
+ */
+
+public class InterfazPlaneacionUrbana extends JFrame{
+	
+	
+	// -----------------------------------------------------------------
+    // Constructor
+    // -----------------------------------------------------------------
+
+    /**
+     * Crea la interfaz del juego planeacion urbana.
+     * @throws Exception Error al cargar los archivos.
+     */
+	public InterfazPlaneacionUrbana() throws Exception
+	{
+        setTitle( "Planeación Urbana" );
+        setSize( 820, 740 );
+        setDefaultCloseOperation( EXIT_ON_CLOSE );
+	}
+	
+	
 
 }
--- n5_planeacionUrbana/source/uniandes/cupi2/planeacionUrbana/interfaz/PanelEstadoDeJuego.java
+++ n5_planeacionUrbana/source/uniandes/cupi2/planeacionUrbana/interfaz/PanelEstadoDeJuego.java
@@ -1,5 +1,38 @@
 package uniandes.cupi2.planeacionUrbana.interfaz;
 
-public class PanelEstadoDeJuego {
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+
+/**
+ * Panel para presentar la información del estado del juego.
+ */
+public class PanelEstadoDeJuego extends JPanel{
+	
+	/**
+     * Crea el panel de estado de juego.
+     */
+    public PanelEstadoDeJuego( )
+    {
+        // Establece el layout como una grilla de 3 filas y 2 columnas
+        setLayout( new GridLayout( 3, 2 ) );
+
+        // Adiciona un marco con título
+        TitledBorder border = new TitledBorder( "Descuentos" );
+        border.setTitleColor( Color.BLUE );
+        setBorder( border );
+
+        // Crea los objetos del panel
+        cbPPago = new JCheckBox( "Pronto pago" );
+        cbSPublico = new JCheckBox( "Servicio público" );
+        cbTCuenta = new JCheckBox( "Traslado de cuenta" );
+
+        // Adiciona los objetos del panel
+        add( cbPPago );
+        add( cbTCuenta );
+        add( cbSPublico );
+    }
 
 }
